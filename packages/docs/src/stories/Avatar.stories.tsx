71ba3e99d8c4b873166c7034bce85337ed9ac6d8
Luan Castheloge <luanccp@gmail.com>
2024-02-23 11:34:55 +0000

    feat: adding avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@luanccp-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/luanccp.png',
+    alt: 'Luan Castheloge',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
