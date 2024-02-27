f63ab036e38736ae38f33d3001940f4fee021c3e
Luan Castheloge <luanccp@gmail.com>
2024-02-27 14:49:46 +0000

    feat: adding visual customization through storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/luanccp.png',
     alt: 'Luan Castheloge',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
