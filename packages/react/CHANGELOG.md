e0e79fabf1fce8714d5b67c60f3f52e8e708039c
Luan Castheloge <luanccp@gmail.com>
2024-02-29 22:57:58 +0000

    feat: adding ref to text input and adjust some styles

--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,12 @@
 # @luanccp-ui/react
 
+## 1.0.2
+
+### Patch Changes
+
+- Adding `ref` to TextInput
+- Adjusting some styles
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -20,7 +20,7 @@ export const CheckboxContainer = styled(Checkbox.Root, {
     backgroundColor: '$primary300',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     border: '2px solid $primary300',
   },
 })
